64b2cb3b0fbfe7b7d5ec91fe95670f65cbfec090
NishchayBeharam <86786660+NishchayBeharam@users.noreply.github.com>
2023-06-01 14:15:11 +0530

    ContactOverlayAdded

---
+++ js/app.js
@@ -0,0 +1,20 @@
+const showContactOverlay = async () => {
+
+    document.getElementById('git-overlay').classList.toggle('display-flex')
+    setTimeout(()=>{
+        document.getElementById('git-overlay').classList.toggle('git-overlay-visible')
+    },100)
+
+}
+
+const hideContactOverlay = async () => {
+    document.getElementById('git-overlay').classList.toggle('git-overlay-visible')
+    setTimeout(()=>{
+        document.getElementById('git-overlay').classList.toggle('display-flex')
+    },200)
+}
+
+document.getElementById('home-section-a-cta-id').addEventListener('click',showContactOverlay)
+document.getElementById('nav-social-icons-item-call').addEventListener('click',showContactOverlay)
+document.getElementById('nav-booking-btn-id').addEventListener('click',showContactOverlay)
+document.getElementById('git-overlay-exit').addEventListener('click',hideContactOverlay)
